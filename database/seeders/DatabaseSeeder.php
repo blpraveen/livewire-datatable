3bc4f81fbb1146882cc2bd1210a02d2d9a63e890
B L Praveen <blpraveen2004@gmail.com>
2023-07-09 20:17:30 +0530

    final commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,7 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-
+        $this->call(CategorySeeder::class);
         \App\Models\Post::factory(200)->create();
     }
 }
